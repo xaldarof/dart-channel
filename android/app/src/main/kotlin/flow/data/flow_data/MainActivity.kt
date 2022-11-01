0f75f0847848c82cf7e36807c6872ec2597689df
Temur Xoldarov <temuruz2003@gmail.com>
2022-11-01 15:19:32 +0500

    initial

---
+++ android/app/src/main/kotlin/flow/data/flow_data/MainActivity.kt
@@ -0,0 +1,6 @@
+package flow.data.flow_data
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/flow_data.dart
@@ -0,0 +1,54 @@
+class FlowData<T> {
+  List<T?> values = [];
+  int bufferSize;
+
+  final List<OnValueChangeCallBack<T?>> _listeners = [];
+
+  void emit(T? value) {
+    values.add(value);
+    if (values.length > bufferSize) {
+      values.removeAt(0);
+    }
+    _invokeListeners();
+  }
+
+  void _invokeListeners() {
+    for (var listener in _listeners) {
+      for (var element in values) {
+        listener.call(element);
+      }
+      if (values.length > bufferSize) {
+        values.removeRange(values.length - bufferSize, values.length - 1);
+      }
+    }
+  }
+
+  T? getFirst() {
+    return values[0];
+  }
+
+  T? getLast() {
+    return values[values.length - 1];
+  }
+
+  void addListener(OnValueChangeCallBack<T?> value) {
+    _listeners.add(value);
+    _invokeListeners();
+  }
+
+  void removeListener(OnValueChangeCallBack<T?> value) {
+    _listeners.remove(value);
+  }
+
+  void clearListeners() {
+    _listeners.clear();
+  }
+
+  FlowData({
+    required this.bufferSize,
+  }) {
+    assert(bufferSize > 0);
+  }
+}
+
+typedef OnValueChangeCallBack<T> = Function(T);
